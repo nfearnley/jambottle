b4fc4e2ad94c148f15457d05540406a74d0c2aa4
Natalie Fearnley <nfearnley@gmail.com>
2020-11-03 04:23:47 -0500

    fixed grouper args

--- jambottle/jambottle.py
+++ jambottle/jambottle.py
@@ -14,7 +14,7 @@ from discord.utils import sleep_until
 # https://stackoverflow.com/questions/3992735/python-generator-that-groups-another-iterable-into-groups-of-n/3992765
 # https://stackoverflow.com/questions/31164731/python-chunking-csv-file-multiproccessing/31170795#31170795
 # https://docs.python.org/3/library/functions.html#iter
-def grouper(n, iterable):
+def grouper(iterable, n):
     """
     >>> list(grouper(3, 'ABCDEFG'))
     [['A', 'B', 'C'], ['D', 'E', 'F'], ['G']]
